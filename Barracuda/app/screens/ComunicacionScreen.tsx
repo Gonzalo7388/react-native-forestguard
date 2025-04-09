5a0c6c2ff5ccbf5cd5ea353c2e7751bbce04e6c7
Gonzalo7388 <201820829@urp.edu.pe>
2025-04-09 22:07:54 +0000

    Guardando progreso actual antes de salir

---
+++ Barracuda/app/screens/ComunicacionScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const ComunicacionScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Pantalla de Comunicación</Text>
+    </View>
+  );
+};
+
+export default ComunicacionScreen;
---
+++ Barracuda/app/screens/EquiposScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const EquiposScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Pantalla de Equipos</Text>
+    </View>
+  );
+};
+
+export default EquiposScreen;
